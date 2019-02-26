aeee05967ba4518ac2c51c0c139f6d920ca7ca77
Unknown <darren@bruning.net.nz>
2019-02-27 12:01:58 +1300

    Works with Nuget

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.dotMemoryUnit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ClassLibrary1
+{
+	public class Class1
+	{
+		public Class1(ITestOutputHelper outputHelper)
+		{
+			// Ensure that dotMemory output appears in test output
+			// https://blog.jetbrains.com/dotnet/2018/10/04/unit-testing-memory-leaks-using-dotmemory-unit/
+			DotMemoryUnitTestOutput.SetOutputMethod(
+				message => outputHelper.WriteLine(message));
+		}
+		
+		[Fact]
+		public void CanRunTestWithDotMemory()
+		{
+			dotMemory.Check(memory =>
+				Assert.Equal(1, 1));
+		}
+	}
+}
